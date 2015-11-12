f919ac8182fee631143fd451c892bd3fc595e164
TK-R <subtkr@gmail.com>
2015-11-12 23:21:23 +0900

    パスワード保存ファイル（デバッグ用）を追加 ファイルからの情報読み出しを実装

--- MFSCraping/MFSCraping/Program.cs
+++ MFSCraping/MFSCraping/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MFSCraping
 {
@@ -10,6 +12,37 @@ namespace MFSCraping
     {
         static void Main(string[] args)
         {
+            var path = Environment.CurrentDirectory + "\\Data\\Auth.xml";
+            string mail, pass;
+
+            // mail pass 
+            if (args.Length >= 2)
+            {
+                mail = args[0];
+                pass = args[1];
+            }
+            else if (File.Exists(path))
+            {
+
+                var doc = XDocument.Load(path);
+                try
+                {
+                    mail = doc.Element("root").Element("mail").Value;
+                    pass = doc.Element("root").Element("pass").Value;
+
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Invalid xml file.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("MFScraping.exe <mail> <pass> or Create Auth.xml");
+                return;
+            }
+
+
 
         }
     }
